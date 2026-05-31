1aaaf3fb3487ff2cf626be51fb9f339788e47ef9
Mshael <shawaf990@gmail.com>
2026-05-31 21:28:17 +0300

    add few comments

--- config.php
+++ config.php
@@ -15,12 +15,10 @@ if (!defined('BASE_URL')) {
     // echo "<pre>"; // Makes the output easy to read
     // print_r($_SESSION); 
     // echo "</pre>";
-
     // Force PHP to show errors
     // ini_set('display_errors', 1);
     // ini_set('display_startup_errors', 1);
     // error_reporting(E_ALL);
-    
     // echo "<script>console.log('PHP says: Redirect would happen here');</script>";   
     
 
